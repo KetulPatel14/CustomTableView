7bb2afc1f7829f611e638b066f595e636f0eb2a8
KetulPatel14 <128503538+KetulPatel14@users.noreply.github.com>
2023-03-28 13:24:45 -0400

    add code

--- CustomTableApplication/CustomTableApplication/ViewController.swift
+++ CustomTableApplication/CustomTableApplication/ViewController.swift
@@ -17,16 +17,35 @@ class ViewController: UIViewController {
         let imageName: String
     }
     
+    let data: [myCell] = [
+    myCell(title: "Item1", imageName: "Image"),
+    myCell(title: "Item2", imageName: "Image"),
+    myCell(title: "Item3", imageName: "Image"),
+    myCell(title: "Item4", imageName: "Image"),
+    myCell(title: "Item5", imageName: "Image"),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myCustomTable.delegate = self
+        myCustomTable.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
     }
-
-
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Selected \(data[indexPath.row].title)")
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let content = data[indexPath.row]
+        let cell = myCustomTable.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
+        cell.cellImage.image = UIImage(named: content.imageName)
+        cell.cellLabel.text = content.title
+        return cell
+    }
 }
 
