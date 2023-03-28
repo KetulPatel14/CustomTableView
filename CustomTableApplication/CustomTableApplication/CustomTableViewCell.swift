c1cfaa9ca204a29abcdc41d30e24f69a62f91035
KetulPatel14 <128503538+KetulPatel14@users.noreply.github.com>
2023-03-28 12:59:12 -0400

    connect image and label

---
+++ CustomTableApplication/CustomTableApplication/CustomTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CustomTableViewCell.swift
+//  CustomTableApplication
+//
+//  Created by Canadore Student on 2023-03-28.
+//  Copyright © 2023 Canadore Student. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cellLabel: UILabel!
+    @IBOutlet weak var cellImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
